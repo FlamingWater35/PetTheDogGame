29f79e3b4b29f8b5e3cbb89db51bbab5813a5d23
unknown <flamingwater69420@gmail.com>
2024-09-10 09:43:41 -0700

    Reset dog position at load

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725985353|38256218';
+const CACHE_VERSION = '1725986578|63315678';
 /** @type {string} */
 const CACHE_PREFIX = 'Pet the Dog-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
