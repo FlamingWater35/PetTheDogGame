07b2efcc343b1ea01fcd60e5e6737a4b84362ca8
unknown <flamingwater69420@gmail.com>
2024-09-10 11:37:00 -0700

    Make save button visible on load

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725993097|96706386';
+const CACHE_VERSION = '1725993361|54892988';
 /** @type {string} */
 const CACHE_PREFIX = 'Pet the Dog-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
